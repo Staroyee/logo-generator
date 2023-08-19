f21808ff92068ba9e526948a699f6769165e93eb
Staroyee <danmasefield@gmail.com>
2023-08-19 23:33:25 +0930

    Added comments to the functionality of the main js files.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+//Import inquirer module.
 const inquirer = require("inquirer");
+//Import createSvg function.
 const createSvg = require("./lib/writeFile") 
 
+//Function to prompt user.
 function prompt() {
 inquirer
   .prompt([
@@ -27,13 +30,16 @@ inquirer
     },
   ])
   .then((data) => {
+    //Use the data to check if the text length is more than three.
     if (data.text.length > 3) {
       console.log("Please enter no more than three characters.");
+      //If the text length is longer than three, run the prompt function again.
       prompt();
     } else {
+      //If criteria is met, run the createSvg function.
       createSvg("./examples/logo.svg", data);
     }
   });
 }
-
+//Run the prompt function when the app is run.
 prompt();
